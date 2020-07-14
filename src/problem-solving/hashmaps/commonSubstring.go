6302c00247870406e585e71673b07629f7cae38e
shishir-bondre <shishir.bondre@narvar.com>
2020-07-14 15:25:18 +0530

    Adds solution for common substring

--- src/problem-solving/hashmaps/commonSubstring.go
+++ src/problem-solving/hashmaps/commonSubstring.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(commonSubstring("shishir", "madhura"))
+}
+
+
+func commonSubstring(source, target string) string{
+	wordsDict := make(map[string]int)
+	for _, value:= range source {
+		wordsDict[string(value)] = 1
+	}
+	for _, value := range target{
+		if _, ok := wordsDict[string(value)]; ok{
+			return "Yes"
+		}
+	}
+	return "No"
+}
